4057e0ea75bfcc8e55ac2bf554ecd4d2556cffc2
kavish <kavish@kavishs-MacBook-Pro.local>
2024-06-01 10:05:04 +0530

    Added updated solution for Challenge 2

--- challenge-2/challenge_2.py
+++ challenge-2/challenge_2.py
@@ -1,5 +1,7 @@
 #Challenge 2: http://www.pythonchallenge.com/pc/def/ocr.html
 
+import re
+
 def read_data(file_path):
     with open(file_path, "r") as f:
         data=f.read()
@@ -14,9 +16,8 @@ def get_item_counts(data):
             item_count[i] += 1
     return item_count
 
-def get_next_challenge_url(item_count):
-    next_page=""
-    for k in item_count:
-        if item_count[k] <= 1:
-            next_page = next_page + k
+
+#Updated solution taken from here for correct url - https://www.hackingnote.com/en/python-challenge-solutions/level-2/index.html
+def get_next_challenge_url(data):
+    next_page = "".join(re.findall("[A-Za-z]", data))
     return "http://www.pythonchallenge.com/pc/def/" + next_page + ".html"
--- challenge-2/main.py
+++ challenge-2/main.py
@@ -1,10 +1,7 @@
 #Challenge 2: http://www.pythonchallenge.com/pc/def/ocr.html
 
 import challenge_2
-from os import name
-
 
 file_path="data.txt"
 data=challenge_2.read_data(file_path)
-item_count=challenge_2.get_item_counts(data)
-print("The next challenge url is - ", challenge_2.get_next_challenge_url(item_count))
+print("The next challenge url is - ", challenge_2.get_next_challenge_url(data))
